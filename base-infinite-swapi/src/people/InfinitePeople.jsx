9f5c1b382dcef5abf2d7d5f347d51236d64080c3
seunghwan <sun1238799@naver.com>
2022-07-27 11:22:30 +0900

    error

--- base-infinite-swapi/src/people/InfinitePeople.jsx
+++ base-infinite-swapi/src/people/InfinitePeople.jsx
@@ -1,13 +1,43 @@
 import InfiniteScroll from "react-infinite-scroller";
 import { Person } from "./Person";
-
+import { useInfiniteQuery } from "react-query";
 const initialUrl = "https://swapi.dev/api/people/";
 const fetchUrl = async (url) => {
   const response = await fetch(url);
   return response.json();
 };
-
+//pageParam= fetchNextPage가 어떻게 보일지 결정함
 export function InfinitePeople() {
+  const { data, fetchNextPage, hasNextPage, isLoading, isError, error } =
+    useInfiniteQuery(
+      "sw-people",
+      ({ pageParam = initialUrl }) => fetchUrl(pageParam),
+      {
+        getNextPageParam: (lastPage) => lastPage.next || undefined,
+      }
+    );
+
+  if (isLoading) {
+    return <div> Loading...</div>;
+  }
+  if (isError) {
+    return <div> Error</div>;
+  }
   // TODO: get data for InfiniteScroll via React Query
-  return <InfiniteScroll />;
+  return (
+    <InfiniteScroll loadMore={fetchNextPage} hasMore={hasNextPage}>
+      {data.pages.map((pageData) => {
+        return pageData.results.map((person) => {
+          return (
+            <Person
+              key={person.name}
+              name={person.name}
+              hairColor={person.hair_color}
+              eyeColor={person.eye_color}
+            ></Person>
+          );
+        });
+      })}
+    </InfiniteScroll>
+  );
 }
